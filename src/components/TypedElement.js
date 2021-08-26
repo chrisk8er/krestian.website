eb2b82240456aa9c03644beaccd20e0b1799c465
krestian <chrisk8er@gmail.com>
2021-08-26 20:12:56 +0800

    update front page

--- src/components/TypedElement.js
+++ src/components/TypedElement.js
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from "react";
-import Typed from "typed.js";
+import React, { useEffect, useState } from 'react';
+import Typed from 'typed.js';
 
 const TypedElement = ({ messages }) => {
   const [option, setOption] = useState({
-    stringsElement: "#typed-strings",
+    stringsElement: '#typed-strings',
     typeSpeed: 30,
     loop: true,
   });
 
   useEffect(() => {
-    const typed = new Typed("#typed", option);
+    const typed = new Typed('#typed', option);
   }, []);
   return (
     <div className="is-flex">
-      <p className="mr-3">I am</p>
+      <p className="mr-1">I am</p>
       <ul id="typed-strings">
         {messages.map((value, index) => (
           <li key={index}>{value}</li>
